6326c7942a3d1daf0c8873464125b2f4f4535a2d
shubhamdixit765 <shubham.dixit_iot18@gla.ac.in>
2019-08-21 23:32:49 +0530

    Calculate the result and print it to the screen.

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -19,9 +19,9 @@ public class Gravity {
         time = sc.nextDouble();
         speed = GRAVITY * time;
         distance = (GRAVITY * time * time) / 2;
-		/* TODO 2:
-		 *   c. Calculate the result and print it to the screen.
-		 * */
-	}
-	// P.S: Refer to the README.md file for the problem statement, input & output.
+        System.out.println("The speed of the object at " + time" seconds after its release is " + speed" and the distance the object has travelled in the " + time" seconds after the relase is " + distance);
+        sc.close();
+    }
+}
+
 }
